b346bc89d8ae60eb1545e6f5ff4575ea34d6bc39
Gideon-D-Adeti <gideondelaliadeti@gmail.com>
2024-05-25 20:57:29 +0000

    Write fibsRec function using recursion

--- script.js
+++ script.js
@@ -18,3 +18,22 @@ function fibs(n) {
 
 // Example usage:
 console.log(fibs(8));  // Output: [0, 1, 1, 2, 3, 5, 8, 13]
+
+function fibsRec(n) {
+    if (n <= 0) {
+        return [];
+    } else if (n === 1) {
+        return [0];
+    } else if (n === 2) {
+        return [0, 1];
+    }
+
+    let fibSequence = fibsRec(n - 1);
+    let nextFib = fibSequence[fibSequence.length - 1] + fibSequence[fibSequence.length - 2];
+    fibSequence.push(nextFib);
+
+    return fibSequence;
+}
+
+// Example usage:
+console.log(fibsRec(8));  // Output: [0, 1, 1, 2, 3, 5, 8, 13]
