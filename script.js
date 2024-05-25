60a9692e3c4b12312771de0f8e5c23b24ae16bc1
Gideon-D-Adeti <gideondelaliadeti@gmail.com>
2024-05-25 21:01:06 +0000

    Test both functions with various inputs

--- script.js
+++ script.js
@@ -1,39 +1,42 @@
-function fibs(n) {
+function fibs (n) {
   if (n <= 0) {
-    return [];
+    return []
   } else if (n === 1) {
-    return [0];
+    return [0]
   } else if (n === 2) {
-    return [0, 1];
+    return [0, 1]
   }
 
-  let fibSequence = [0, 1];
+  const fibSequence = [0, 1]
   for (let i = 2; i < n; i++) {
-    let nextFib = fibSequence[i - 1] + fibSequence[i - 2];
-    fibSequence.push(nextFib);
+    const nextFib = fibSequence[i - 1] + fibSequence[i - 2]
+    fibSequence.push(nextFib)
   }
 
-  return fibSequence;
+  return fibSequence
 }
 
-// Example usage:
-console.log(fibs(8));  // Output: [0, 1, 1, 2, 3, 5, 8, 13]
+function fibsRec (n) {
+  if (n <= 0) {
+    return []
+  } else if (n === 1) {
+    return [0]
+  } else if (n === 2) {
+    return [0, 1]
+  }
 
-function fibsRec(n) {
-    if (n <= 0) {
-        return [];
-    } else if (n === 1) {
-        return [0];
-    } else if (n === 2) {
-        return [0, 1];
-    }
+  const fibSequence = fibsRec(n - 1)
+  const nextFib = fibSequence[fibSequence.length - 1] + fibSequence[fibSequence.length - 2]
+  fibSequence.push(nextFib)
 
-    let fibSequence = fibsRec(n - 1);
-    let nextFib = fibSequence[fibSequence.length - 1] + fibSequence[fibSequence.length - 2];
-    fibSequence.push(nextFib);
-
-    return fibSequence;
+  return fibSequence
 }
 
-// Example usage:
-console.log(fibsRec(8));  // Output: [0, 1, 1, 2, 3, 5, 8, 13]
+// Test both functions with various inputs
+const testCases = [0, 1, 2, 5, 8, 10]
+
+testCases.forEach(num => {
+  console.log(`fibs(${num}):`, fibs(num))
+  console.log(`fibsRec(${num}):`, fibsRec(num))
+  console.log('------------------------')
+})
